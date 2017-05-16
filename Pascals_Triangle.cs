75ff4629c5455e0ea07feea8a1e2ef6e266b6e74
Michal Walawender <michal.walawender@gmail.com>
2017-05-17 00:16:00 +0200

    Pascals_Triangle
    Draw Pascal's triangle

---
+++ Pascals_Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pascals_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj wysokość");
+            string input = Console.ReadLine();
+            int to = Convert.ToInt16(input);
+            int spaces = to / 5 * 2 + 1;
+            for (int i = 0; i < to; i++)
+            {
+                Console.Write(new String(' ', (spaces + 1) / 2 * ((to - 1) - i)));
+                int l = 1;
+                for (int j = 0; j<=i; j++)
+                {
+                    Console.Write(l);
+                    Console.Write(new string(' ',spaces + 1 - l.ToString().Length));
+                    l = l * (i - j) / (j + 1);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
